0e2d3b1f7ac3f564e145d293041e7959a92a4460
Jonny Ortiz <j0nny.0rt1z@yahoo.com>
2022-05-31 13:08:04 -0400

    updated verbiage in Benes

--- src/components/Benes.js
+++ src/components/Benes.js
@@ -4,7 +4,7 @@ function Benes() {
    return (
       <div className="benes">
          <h3>Individually customized salon and spa experience</h3>
-         <h3>Eco-friendly, handmade products</h3>
+         <h3>Natural, handmade products</h3>
          <h3>Serving the Tampa Bay area</h3>
       </div>
    );
